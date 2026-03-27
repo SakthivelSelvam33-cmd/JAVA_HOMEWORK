9c850fa3dc1475847cdc3be3a0455599d44a20f5
SakthivelSelvam33 <sakthivelselvam33@gmail.com>
2026-03-27 03:16:28 -0700

    About eventhandling

---
+++ ReverseDigits.java
@@ -0,0 +1,27 @@
+
+package reversedigits;
+
+import java.util.Scanner;
+
+
+public class ReverseDigits {
+
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();   
+        int rev = 0;
+
+        while (num != 0) 
+        {
+            int digit = num % 10;     
+            rev = rev * 10 + digit;   
+            num = num / 10;           
+        }
+
+        System.out.println(rev);
+    }
+    }
+    
+
